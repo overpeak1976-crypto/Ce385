5460cebffb4e2349753dbdc05818a9ffb21cf13e
overpeak1976-crypto <overpeak1976@gmail.com>
2026-03-01 23:20:53 +0700

    update

--- week_6/src/index.ts
+++ week_6/src/index.ts
@@ -52,3 +52,128 @@ app.put('/user/:id', async (req, res) => {
         res.status(500).json({ error: 'Faild to update user '});
     }
 })
+
+//////. API ตาราง (User). //////
+
+// GET /users/email/:email
+app.get('/users/email/:email', async (req, res) => {
+    const { email } = req.params;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email: email },
+        });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.json(user);
+    }
+    catch (error) {
+        res.status(500).json({ error: "Failed to fetch user" });
+    }
+});
+
+//DELETE /users/:id
+app.delete('/users/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await prisma.user.delete({
+            where: { Userid: id }
+        });
+
+        res.json({ message: "User deleted successfully" });
+    }
+    catch (error) {
+        res.status(500).json({ error: "Failed to delete user" });
+    }
+});
+
+
+//////. API ตาราง (Post). //////
+
+//POST /posts
+app.post('/posts', async (req, res) => {
+    const { title, content, authorId } = req.body;
+
+    try {
+        const post = await prisma.post.create({
+            data: {
+                title,
+                content,
+                authorId
+            }
+        });
+
+        res.status(201).json(post);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to create post" });
+    }
+});
+
+//GET /posts
+app.get('/posts', async (req, res) => {
+    try {
+        const posts = await prisma.post.findMany();
+        res.json(posts);
+    }
+    catch (error) {
+        res.status(500).json({ error: "Failed to fetch posts" });
+    }
+});
+
+//GET /posts/:id
+app.get('/posts/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const post = await prisma.post.findUnique({
+            where: { postId: id },
+        });
+
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        res.json(post);
+    }
+    catch (error) {
+        res.status(500).json({ error: "Failed to fetch post" });
+    }
+});
+
+//PUT /posts/:id
+app.put('/posts/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    try {
+        const post = await prisma.post.update({
+            where: { postId: id },
+            data: { title, content }
+        });
+
+        res.json(post);
+    }
+    catch (error) {
+        res.status(500).json({ error: "Failed to update post" });
+    }
+});
+
+//DELETE /posts/:id
+app.delete('/posts/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await prisma.post.delete({
+            where: { postId: id },
+        });
+
+        res.json({ message: "Post deleted successfully" });
+    }
+    catch (error) {
+        res.status(500).json({ error: "Failed to delete post" });
+    }
+});
